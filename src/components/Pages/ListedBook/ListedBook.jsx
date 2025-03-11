e459c2ce91f1a0eaa745ab77a65f59412ae6471f
mahmud-reza-rafsun <rafsun16.it@gmail.com.com>
2025-03-11 22:18:27 +0600

    project setup

---
+++ src/components/Pages/ListedBook/ListedBook.jsx
@@ -0,0 +1,9 @@
+const ListedBook = () => {
+  return (
+    <div>
+      <h2>Listed Book</h2>
+    </div>
+  );
+};
+
+export default ListedBook;
---
+++ src/components/Pages/ReadToPage/ReadToPage.jsx
@@ -0,0 +1,9 @@
+const ReadToPage = () => {
+  return (
+    <div>
+      <h2>Read TO page</h2>
+    </div>
+  );
+};
+
+export default ReadToPage;
